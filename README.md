6c93723c54050c204cee867fa7b09f41d024674a
eXOffice <sumontobonti@gmail.com>
2023-03-20 21:25:01 +0600

    Starting Project

---
+++ README.md
@@ -0,0 +1,7 @@
+# bitcoin_ticker
+
+A Bitcoin Ticker  Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RunApp());
+}
+
+class RunApp extends StatelessWidget {
+  const RunApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
